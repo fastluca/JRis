f74e74733ebc51459cbc8f31b2546edb98a0439f
Urs Joss <u.joss@calltrade.ch>
2019-12-07 13:26:33 +0100

    Bump kordamp gradle plugin from 0.30.3 to 0.30.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 
     plugins {
         kotlin("jvm") version "1.3.61"
-        val kordampPluginVersion = "0.30.3"
+        val kordampPluginVersion = "0.30.4"
         id("org.kordamp.gradle.project") version kordampPluginVersion
         id("org.kordamp.gradle.integration-test") version kordampPluginVersion
         id("org.kordamp.gradle.guide") version kordampPluginVersion
@@ -27,7 +27,7 @@ buildscript {
         jcenter()
         gradlePluginPortal()
     }
-    val kordampPluginVersion = "0.30.3"
+    val kordampPluginVersion = "0.30.4"
     dependencies {
         classpath("org.kordamp.gradle:settings-gradle-plugin:$kordampPluginVersion")
     }
