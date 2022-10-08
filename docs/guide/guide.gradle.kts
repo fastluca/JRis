282f7b6d8abfb4395e2de614d1987f41f0e825f1
Urs Joss <urs.joss@gmx.ch>
2022-10-09 01:17:02 +0200

    fix guide

--- docs/guide/guide.gradle.kts
+++ docs/guide/guide.gradle.kts
@@ -3,56 +3,52 @@ import java.time.Year
 
 plugins {
     alias(libs.plugins.asciidoctorConvert)
-    alias(libs.plugins.asciidoctorPdf)
     alias(libs.plugins.gitPublish)
 }
 
 tasks {
+    val builtGuideDir = file("${project.buildDir}/guide")
     withType<AsciidoctorTask> {
         sourceDir(file("src/docs/asciidoc/"))
         setBaseDir(file("src/docs/asciidoc/"))
         sources(delegateClosureOf<PatternSet> { include("index.adoc") })
-        setOutputDir(file("build/docs"))
         options(mapOf("doctype" to "book"))
-        outputOptions {
-            setBackends(listOf("html5", "pdf"))
-            attributes = mapOf(
-                "includedir" to "$sourceDir/",
-                "imagesdir" to "$sourceDir/img",
-                "source-highlighter" to "coderay",
-                "coderay-linenums-mode" to "table",
-                "rootdir" to project.rootProject.projectDir.absolutePath,
-                "allow-url-read" to "",
-                "toc" to "left",
-                "icons" to "font",
-                "encoding" to "utf-8",
-                "sectlink" to "true",
-                "sectanchors" to "true",
-                "numbered" to "true",
-                "linkattrs" to "true",
-                "linkcss" to "true",
-                "project-title" to "KRis -- Library for reading/writing RIS files",
-                "project-inception-year" to "2017",
-                "project-copyright-year" to Year.now().value,
-                "project-author" to "Gianluca Colaianni, Urs Joss",
-                "project-url" to "https://github.com/ursjoss/KRis",
-                "project-sccm" to "https://github.com/ursjoss/KRis.git",
-                "project-issue-tracker" to "https://github.com/ursjoss/KRis/issues",
-                "project-group" to project.group,
-                "project-version" to project.version,
-                "project-name" to project.rootProject.name,
-                "gradle-version" to project.gradle.gradleVersion,
-            )
-        }
+        outputOptions {setBackends(listOf("html5"))}
+        attributes = mapOf(
+            "includedir" to "$sourceDir/",
+            "imagesdir" to "$sourceDir/img",
+            "source-highlighter" to "prettify",
+            "rootdir" to project.rootProject.projectDir.absolutePath,
+            "allow-url-read" to "",
+            "toc" to "left",
+            "icons" to "font",
+            "encoding" to "utf-8",
+            "sectlink" to "true",
+            "sectanchors" to "true",
+            "numbered" to "true",
+            "linkattrs" to "true",
+            "linkcss" to "true",
+            "project-title" to "KRis -- Library for reading/writing RIS files",
+            "project-inception-year" to "2017",
+            "project-copyright-year" to Year.now().value,
+            "project-author" to "Gianluca Colaianni, Urs Joss",
+            "project-url" to "https://github.com/ursjoss/KRis",
+            "project-sccm" to "https://github.com/ursjoss/KRis.git",
+            "project-issue-tracker" to "https://github.com/ursjoss/KRis/issues",
+            "project-group" to project.group,
+            "project-version" to project.version,
+            "project-name" to project.rootProject.name,
+            "gradle-version" to project.gradle.gradleVersion,
+        )
     }
 
     val asciidoctor by existing
     val createGuide = register<Copy>("createGuide") {
         group = "Documentation"
         description = "Creates an Asciidoctor based guide."
-        dependsOn(asciidoctor)
-        destinationDir = file("${project.buildDir}/guide")
         from(project.tasks.asciidoctor.get().outputDir)
+        destinationDir = builtGuideDir
+        dependsOn(asciidoctor)
     }
 
     named("gitPublishCommit") {
